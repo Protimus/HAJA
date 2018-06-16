eee6440a60867ee90c5fdf3c04e20d8e7c0123da
Rafael Gorges <rafaelgorges@outlook.com>
2018-06-15 22:17:27 -0300

    Modificações para exibir os outros gráficos e mudanças gerais na página principal

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { TemperaturaService } from '../../temperatura.service';
+import { VelocidadeService } from '../../velocidade.service';
 import { Chart } from 'chart.js';
+import { interval } from 'rxjs';
 
 @Component({
     selector: 'app-dashboard',
@@ -15,54 +17,108 @@ export class DashboardComponent implements OnInit {
 
     chart = []; // This will hold our chart info
 
-    constructor(private _temperatura: TemperaturaService) {}
+    constructor(
+      private _temperatura: TemperaturaService,
+      //private _velocidade: VelocidadeService
+    ) {}
 
     ngOnInit() {
-        this._temperatura.dailyForecast()
-          .subscribe(res => {
-            let temp_max = res['list'].map(res => res.main.temp_max);
-            let temp_min = res['list'].map(res => res.main.temp_min);
-            let alldates = res['list'].map(res => res.dt)
+      this.TemperaturaGrafico();
+      this.VelocidadeGrafico();
+    }
 
-            let weatherDates = []
-            alldates.forEach((res) => {
-                let jsdate = new Date(res * 1000)
-                weatherDates.push(jsdate.toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric' }))
-            })
+    public TemperaturaGrafico(){
+      this._temperatura.dailyForecast()
+        .subscribe(res => {
+        let temp_max = res['list'].map(res => res.main.temp_max);
+        let temp_min = res['list'].map(res => res.main.temp_min);
+        let alldates = res['list'].map(res => res.dt)
 
-            this.chart = new Chart('canvas', {
-              type: 'line',
-              data: {
-                labels: weatherDates,
-                datasets: [
-                  { 
-                    data: temp_max,
-                    borderColor: "red",
-                    fill: false
-                  },
-                  { 
-                    data: temp_min,
-                    borderColor: "green",
-                    fill: false
-                  },
-                ]
-              },
-              options: {
-                legend: {
-                  display: false
-                },
-                scales: {
-                  xAxes: [{
-                    display: true
-                  }],
-                  yAxes: [{
-                    display: true
-                  }],
-                }
-              }
-            });
+        let weatherDates = []
+        alldates.forEach((res) => {
+        let jsdate = new Date(res * 1000)
+        weatherDates.push(jsdate.toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric' }))
+        })
 
-          })
+        this.chart = new Chart('canvas', {
+          type: 'line',
+          data: {
+            labels: weatherDates,
+            datasets: [
+            { 
+              data: temp_max,
+              borderColor: "red",
+              fill: false
+            },
+            { 
+              data: temp_min,
+              borderColor: "green",
+              fill: false
+            },
+            ]
+          },
+          options: {
+            legend: {
+              display: false
+            },
+            scales: {
+              xAxes: [{
+                display: true
+              }],
+              yAxes: [{
+                display: true
+              }],
+            }
+          }
+        });
+      })
+    }
+
+    public VelocidadeGrafico(){
+      this._temperatura.dailyForecast()
+        .subscribe(res => {
+        let temp_max = res['list'].map(res => res.main.temp_max);
+        let temp_min = res['list'].map(res => res.main.temp_min);
+        let alldates = res['list'].map(res => res.dt)
+
+        let weatherDates = []
+        alldates.forEach((res) => {
+        let jsdate = new Date(res * 1000)
+        weatherDates.push(jsdate.toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric' }))
+        })
+
+        this.chart = new Chart('canvas', {
+          type: 'line',
+          data: {
+            labels: weatherDates,
+            datasets: [
+            { 
+              data: temp_max,
+              borderColor: "red",
+              fill: false
+            },
+            { 
+              data: temp_min,
+              borderColor: "green",
+              fill: false
+            },
+            ]
+          },
+          options: {
+            legend: {
+              display: false
+            },
+            scales: {
+              xAxes: [{
+                display: true
+              }],
+              yAxes: [{
+                display: true
+              }],
+            }
+          }
+        });
+      })
     }
 
     public closeAlert(alert: any) {
